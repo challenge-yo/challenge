0f747c9322aa22afa976b663088003addc00c38c
KC Hansen <kc.x.hansen@gmail.com>
2018-03-22 15:56:02 -0600

    DB created

---
+++ db/insertRelationships.sql
@@ -0,0 +1,6 @@
+insert into relationships (userID1,
+userID2,
+relationshipStatus,
+isMember
+)
+values ($1,$2,$3,$4)
---
+++ db/insertUser.sql
@@ -0,0 +1,8 @@
+insert into users ( facebook_id,
+userName,
+firstName,
+lastName,
+img,
+score
+)
+values ($1,$2,$3,$4,$5,$6)
