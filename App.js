3dc6c8085b514c84244cca1450acbd0d139215d8
Brett Johnson <k.bretton.johnson@gmail.com>
2018-03-22 16:12:38 -0600

    sure thing

--- App.js
+++ App.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
@@ -11,16 +5,11 @@ import {
   Text,
   View
 } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import axios from 'axios'
 
 type Props = {};
 export default class App extends Component<Props> {
+    
   render() {
     return (
       <View style={styles.container}>
@@ -47,9 +36,5 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+ 
 });
